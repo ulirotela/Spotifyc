6ba5c44287c724ad4244b767d7cf294c5585a77e
ulirotela <62649611+ulirotela@users.noreply.github.com>
2022-04-12 15:28:55 +0100

    now we issue a JWT token

--- authenticator/main.py
+++ authenticator/main.py
@@ -1,10 +1,36 @@
-from flask import Flask
-
+from flask import Flask, request, Response
+import jwt
+from datetime import timedelta, datetime
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!<p>"
+users = [
+    {'username': 'uli', 'password': 'mypassword'},
+    {'username': 'eve', 'password': 'mypassword2'},
+    {'username': 'caio', 'password': 'mypassword3'},
+]
+
+@app.route("/authenticate", methods = ['POST'])
+def authenticate():
+    try:
+        body = request.get_json()
+        username = body['username']
+        password = body['password']
+        # Check if the user is authorized:
+        for user in users:
+            if user['username'] == username and user['password'] == password:
+                # Authentication succeeded
+                claims = {
+                    "iss": "cct",
+                    "sub": username,
+                    "aud": "users",
+                    "exp": (datetime.now() + timedelta(minutes = 5)).timestamp(),
+                    "iat": datetime.now().timestamp(),
+                    "role": "user"
+                }
+                return {'token': jwt.encode(claims, "this-is-a-secret", algorithm="HS256")}                
+    except:
+        return Response('Unauthorized', 401)
+    return Response('Unauthorized', 401)
 
 if __name__ == '__main__':
     app.run()
